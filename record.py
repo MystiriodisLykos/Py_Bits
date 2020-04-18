a80fa059b3c33ea91bbae8890ceea42559ec557b
Brendan DeLeeuw <mystiriodislykos@gmail.com>
2020-04-17 21:34:07 -0600

    Adding an 'over' implementation lens now takes a mapper function, the intent is that it is an explicit fmap     view has been updated with this change The lenses created in _make_record are now the result of an inner function     so that the i and field variables are correct for each lens lenses are now just the name of the field they are accessing

--- record.py
+++ record.py
@@ -11,11 +11,13 @@ def _make_record(nm_tpl, bases = ()):
     nm_tpl_bases = functools.reduce(append_unique, bases, nm_tpl_bases)
     nm_tpl.__bases__ = nm_tpl_bases
     for i, field in enumerate(nm_tpl._fields):
-        def lens(func, record):
-            value = record[i]
-            new_value = func(value)(record)
-            return new_value
-        setattr(nm_tpl, f'{field}_lens', lens)
+        def inner(i, field):
+            def lens(func, record, mapper):
+                value = record[i]
+                new_value = func(value)
+                return mapper(record, new_value, field)
+            return lens
+        setattr(nm_tpl, field, inner(i, field))
     return nm_tpl
 
 
@@ -28,4 +30,8 @@ class Record(typing.NamedTuple, metaclass = RecordMeta):
     _root = True
     
     def view(self, lens):
-        return lens(lambda v: (lambda *args: v), self)
+        return lens(lambda v: v, self, lambda r, v, f: v)
+
+    def over(self, lens, f):
+        f_ = f if callable(f) else lambda v: f
+        return lens(lambda v: f_(v), self, lambda r, v, f: r._replace(**{f: v}))
