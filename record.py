d191a56a6e6caabb6f78b611ea20b53658b39481
Brendan DeLeeuw <mystiriodislykos@gmail.com>
2020-04-18 21:35:42 -0600

    Changing the lens function map the record replace function over the results of the function passed to the lens. Changed view and over for this new version of lens. Removed accessing record values by index. Always accessing by name now.

--- record.py
+++ record.py
@@ -10,14 +10,13 @@ def _make_record(nm_tpl, bases = ()):
     nm_tpl_bases = append_unique(nm_tpl_bases, Record)
     nm_tpl_bases = functools.reduce(append_unique, bases, nm_tpl_bases)
     nm_tpl.__bases__ = nm_tpl_bases
-    for i, field in enumerate(nm_tpl._fields):
-        def inner(i, field):
-            def lens(func, record, mapper):
-                value = record[i]
-                new_value = func(value)
-                return mapper(record, new_value, field)
+    for field in nm_tpl._fields:
+        def inner(field):
+            def lens(func, mapper, record):
+                return mapper(lambda v: record._replace(**{field: v}),
+                                func(getattr(record, field)))
             return lens
-        setattr(nm_tpl, field, inner(i, field))
+        setattr(nm_tpl, f'{field}_lens', inner(field))
     return nm_tpl
 
 
@@ -25,13 +24,14 @@ class RecordMeta(typing.NamedTupleMeta):
     def __new__(mcs, typename, bases, namespace):
         nm_tpl = super().__new__(mcs, typename, bases, namespace)
         return nm_tpl if namespace.get('_root') else _make_record(nm_tpl, bases)
-     
+
+
 class Record(typing.NamedTuple, metaclass = RecordMeta):
     _root = True
     
     def view(self, lens):
-        return lens(lambda v: v, self, lambda r, v, f: v)
+        return lens(lambda v: v, lambda f, v: v, self)
 
     def over(self, lens, f):
         f_ = f if callable(f) else lambda v: f
-        return lens(lambda v: f_(v), self, lambda r, v, f: r._replace(**{f: v}))
+        return lens(lambda v: f_(v), lambda f, v: f(v), self)
