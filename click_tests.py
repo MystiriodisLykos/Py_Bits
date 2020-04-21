79dc4a3767c619f8ecd3a2107a06de178b00acd7
Brendan DeLeeuw <mystiriodislykos@gmail.com>
2020-04-20 22:49:21 -0600

    Figured out how to edit the click context from the group object

--- click_tests.py
+++ click_tests.py
@@ -2,27 +2,27 @@ import click
 
 class TestGroup(click.Group):
     def __init__(self, *args, **kwargs):
-        print('TestGroup, self')
-        # print(args, kwargs)
         super().__init__(*args, **kwargs)
-        self._test = 'X'
-    def get_command(self, ctx, cmd_name):
-        command = super().get_command(ctx, cmd_name)
-        print(ctx.scope())
-        return command
+        self._test = {'Test Data': 'Test String'}
+    def make_context(self, *args, **kwargs):
+        obj = kwargs.get('obj', {})
+        obj.update(self._test)
+        kwargs['obj'] = obj
+        res = super().make_context(*args, **kwargs)
+        return res
+
 
 @click.command(cls = TestGroup)
 @click.pass_context
 def cli(ctx):
-    # with open('entries.txt', 'a') as file:
-    ctx.obj = 'ctx test'
+    pass
 
 @cli.command()
 @click.argument('entry', nargs=-1)
 @click.option('-f', type = click.File('a'), default = 'test')
 @click.pass_context
 def start(state, entry, f):
-    pass
+    print(state.obj)
 
 if __name__ == '__main__':
     cli()
