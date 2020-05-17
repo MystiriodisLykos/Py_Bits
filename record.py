0b2e96391991ffb81a1ee26e0481c244a4473391
Brendan DeLeeuw <mystiriodislykos@gmail.com>
2020-05-17 15:44:15 -0600

    Rename record to Product and adding Sum

--- record.py
+++ record.py
@@ -1,9 +1,33 @@
 import functools
 import typing
 
+import pprint
+
+def deep_tuple(v):
+    if hasattr(v, 'deep_tuple'):
+      return v.deep_tuple()
+    if hasattr(v, 'items'):
+      return tuple((k, deep_tuple(v)) for k, v in v.items())
+
+    try:
+      if len(v) != 1:
+        return tuple(deep_tuple(v_) for v_ in v)
+      return (v[0],)
+    except TypeError as e:
+      if 'cell' in str(e):
+        try:
+          contents = v.cell_contents
+        except ValueError as e:
+          return ()
+        return deep_tuple(contents)
+      pass
+
+    return v
+
+
 class Cache:
     @staticmethod
-    def class_init(cls):
+    def class_new(cls):
         class Cached(cls):
             @Cache.function
             def __new__(cls, *args, **kwargs):
@@ -14,7 +38,7 @@ class Cache:
     def function(func):
         @functools.wraps(func)
         def wrapper(*args, **kwargs):
-            key = tuple(args) + tuple(kwargs)
+            key = deep_tuple(args) + deep_tuple(kwargs)
             if key in wrapper.cache:
                 return wrapper.cache[key]
             res = func(*args, **kwargs)
@@ -23,35 +47,42 @@ class Cache:
         wrapper.cache = {}
         return wrapper
 
+
+@Cache.function
 def append_unique(iter_, val):
     return iter_ if val in iter_ else iter_ + iter_.__class__([val])
 
-def _make_record(nm_tpl, bases = ()):
+
+def _make_product(nm_tpl, bases = ()):
     nm_tpl_bases = nm_tpl.__bases__
-    nm_tpl_bases = append_unique(nm_tpl_bases, Record)
+    nm_tpl_bases = append_unique(nm_tpl_bases, Product)
     nm_tpl_bases = functools.reduce(append_unique, bases, nm_tpl_bases)
     nm_tpl.__bases__ = nm_tpl_bases
     for field in nm_tpl._fields:
         def inner(field):
-            def lens(func, mapper, record):
-                return mapper(lambda v: record._replace(**{field: v}),
-                                func(getattr(record, field)))
+            def lens(func, mapper, product):
+                return mapper(lambda v: product._replace(**{field: v}),
+                                func(getattr(product, field)))
             return lens
         setattr(nm_tpl, f'{field}_lens', inner(field))
-    return Cache.class_init(nm_tpl)
+    return Cache.class_new(nm_tpl)
 
 
-class RecordMeta(typing.NamedTupleMeta):
+class ProductMeta(typing.NamedTupleMeta):
     def __new__(mcs, typename, bases, namespace):
         nm_tpl = super().__new__(mcs, typename, bases, namespace)
-        return nm_tpl if namespace.get('_root') else _make_record(nm_tpl, bases)
+        return nm_tpl if namespace.get('_root') else _make_product(nm_tpl, bases)
 
 
-class Record(typing.NamedTuple, metaclass = RecordMeta):
+class Product(typing.NamedTuple, metaclass = ProductMeta):
     _root = True
-    def __new__(cls, *args, bases = (), **kwargs):
+    def __new__(cls, *args, bases = (), namespace = None, **kwargs):
         nm_tpl = super().__new__(cls, *args, **kwargs)
-        return _make_record(nm_tpl, bases)
+        nm_tpl = _make_product(nm_tpl, bases)
+        if namespace:
+          for k,v in namespace.items():
+            setattr(nm_tpl, k, v)
+        return nm_tpl
 
     @Cache.function
     def view(self, lens):
@@ -66,40 +97,57 @@ class Record(typing.NamedTuple, metaclass = RecordMeta):
         return self.over(lens, lambda _: v)
 
 
-def tests():
-    def lens_tests(record):
+class SumMeta(type):
+  def __new__(mcs, typename, bases, namespace):
+    values = {}
+    if '__annotations__' in namespace:
+      annotations = namespace.pop('__annotations__')
+      for annotation, type_ in annotations.items():
+        if issubclass(type_, Product) or issubclass(type_, Sum):
+          values[annotation] = type_
+        else:
+          values[annotation] = Product(annotation, [(annotation.lower(), type_)], namespace = namespace)
+
+    namespace.update(values)
+    return super().__new__(mcs, typename, bases, namespace)
+
+class Sum(metaclass = SumMeta):
+  _root = True
+
+def product_tests():
+    def lens_tests(product):
         v1, v2, v3, v4 = 101010, 202020, 303030, 404040
-        t1 = record(v1, v2, v3)
+        t1 = product(v1, v2, v3)
         assert t1.x is v1,  'Regular named tuple access returns correct value'
         assert t1[0] is v1,  'Regular named tuple sub index returns correct value'
 
-        assert t1.view(record.y_lens) is v2,  'View with built lens returns correct value'
+        assert t1.view(product.y_lens) is v2,  'View with built lens returns correct value'
 
-        t2 = t1.set(record.y_lens, v4)
+        t2 = t1.set(product.y_lens, v4)
         assert t2.y is v4,  'Set produces correct value'
         assert t2.x is v1,  'Set leaves references to non-set values'
 
-        t2 = t1.over(record.y_lens, lambda v: v*2)
+        t2 = t1.over(product.y_lens, lambda v: v*2)
         assert t2.y == t1.y*2,  'Over sets correct value'
         assert t2.x is v1,  'Over leaves references to non-set values'
 
-    def cache_tests(record):
+    def cache_tests(product):
         v1, v2, v3, v4 = 101010, 202020, 303030, 404040
-        t1 = record(v1, v2, v3)
-        t2 = record(v1, v2, v3)
+        t1 = product(v1, v2, v3)
+        t2 = product(v1, v2, v3)
         assert t1 is t2,  'Creation is cached'
         times_two = lambda v: v*2
-        t3 = t1.over(record.y_lens, times_two)
-        t4 = t1.over(record.y_lens, times_two)
+        t3 = t1.over(product.y_lens, times_two)
+        t4 = t1.over(product.y_lens, times_two)
         assert t3 is t4,  'Over is cached'
 
-    def mixin_tests(record):
-          assert isinstance(record(1), Mixin),  'Supports Mixin classing'
-          assert hasattr(record(1), 'mixin_method'),  'Mixin methods are carried'
-          assert hasattr(record(1), 'class_method'),  'Mixin class methods are carried'
-          assert hasattr(record(1), 'static_method'),  'Mixin static methods are carried'
+    def mixin_tests(product):
+          assert isinstance(product(1), Mixin),  'Supports Mixin classing'
+          assert hasattr(product(1), 'mixin_method'),  'Mixin methods are carried'
+          assert hasattr(product(1), 'class_method'),  'Mixin class methods are carried'
+          assert hasattr(product(1), 'static_method'),  'Mixin static methods are carried'
 
-    class TestClass(Record):
+    class TestClass(Product):
         x: int
         y: int
         z: int
@@ -107,7 +155,7 @@ def tests():
     lens_tests(TestClass)
     cache_tests(TestClass)
 
-    TestFunc = Record('TestFunc', (('x', int), ('y', int), ('z', int)))
+    TestFunc = Product('TestFunc', (('x', int), ('y', int), ('z', int)))
 
     lens_tests(TestFunc)
     cache_tests(TestFunc)
@@ -122,9 +170,28 @@ def tests():
         def static_method(v):
             pass
 
-    class Mixin_Class_Test(Mixin, Record):
+    class Mixin_Class_Test(Mixin, Product):
         v1: int
-    Mixin_Func_Test = Record('Mixin_Func_Test', [('v1', int)], bases = (Mixin, ))
+    Mixin_Func_Test = Product('Mixin_Func_Test', [('v1', int)], bases = (Mixin, ))
 
     mixin_tests(Mixin_Class_Test)
     mixin_tests(Mixin_Func_Test)
+
+def sum_tests():
+  class S1(Sum):
+    X: int
+    Y: str
+
+  assert not hasattr(S1, '__annotations__'),  'Annotations removed'
+  assert issubclass(S1.X, Product),  'Creates Products when needed'
+
+  class P1(Product):
+    v1: int
+    v2: str
+
+  class S2(Sum):
+    P: P1
+    S: S1
+
+  assert S2.P is P1,  'Can have products'
+  assert S2.S is S1,  'Can have sums'
